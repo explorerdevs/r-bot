ab8edd277b297ede0bbd35236db08c7b04c368a8
Prince Muel <vansomecsam@gmail.com>
2022-12-05 12:11:56 +0100

    feat: setup discord events

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
+import { config } from 'dotenv';
 
-dotenv.config();
+config();
 
-const TOKEN = process.env.DISCORD_TOKEN;
+const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 // Require the necessary discord.js classes
-import { Client } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { TOKEN } from './config/index.js';
 
 // Create a new client instance
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`${c.user.username} is ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  console.log(message.content);
+  console.log(message.createdAt.toDateString());
+  console.log(message.author.tag);
+});
+
+client.on(Events.GuildMemberAdd, (member) => {
+  console.log(member.displayName);
+});
 
 // Log in to Discord with your client's token
 client.login(TOKEN);
